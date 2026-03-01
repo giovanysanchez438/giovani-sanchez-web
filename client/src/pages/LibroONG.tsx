98937ee4160e274d8170fb74e754102406565ce1
Giovani sanchez vargas <giovanysanchez438@gmail.com>
2026-03-01 10:29:52 -0500

    Add LibroONG component with embedded iframe

---
+++ client/src/pages/LibroONG.tsx
@@ -0,0 +1,16 @@
+export default function LibroONG() {
+  return (
+    <div style={{ margin: 0, padding: 0, height: "100vh", overflow: "hidden" }}>
+      <iframe
+        src="/libro-finanzas-ong.html"
+        style={{
+          width: "100%",
+          height: "100vh",
+          border: "none",
+          display: "block",
+        }}
+        title="El Arte de Captar Fondos - Giovani Sánchez V."
+      />
+    </div>
+  );
+}
