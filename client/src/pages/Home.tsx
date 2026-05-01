04ffc67d8c4409dd1ea23808cf79111c66503d3e
Giovani sanchez vargas <giovanysanchez438@gmail.com>
2026-05-01 15:03:20 -0500

    Refactor Home component to include new sections

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -1,13 +1,33 @@
 import Hero from "@/components/Hero";
 import Services from "@/components/Services";
+import Principles from "@/components/Principles"; // Expertise
 import Experience from "@/components/Experience";
+import Contact from "@/components/Contact";
 
 export default function Home() {
   return (
-    <main>
+    <main className="flex flex-col min-h-screen bg-white">
       <Hero />
-      <Services />
-      <Experience />
+      
+      {/* Sección de Servicios */}
+      <section id="servicios">
+        <Services />
+      </section>
+
+      {/* Sección de Expertise (Principios) */}
+      <section id="expertise">
+        <Principles />
+      </section>
+
+      {/* Sección de Experiencia Profesional */}
+      <section id="experiencia" className="bg-slate-50">
+        <Experience />
+      </section>
+
+      {/* Sección de Contacto */}
+      <section id="contacto">
+        <Contact />
+      </section>
     </main>
   );
 }
