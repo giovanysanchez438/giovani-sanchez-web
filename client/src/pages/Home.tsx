b2e819dd0d0f0f77c14eff1e48f167407f2c8af7
Giovani sanchez vargas <giovanysanchez438@gmail.com>
2026-05-01 14:51:39 -0500

    Refactor Home component structure and sections

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -6,26 +6,12 @@ import Contact from "@/components/Contact";
 
 export default function Home() {
   return (
-    <main className="flex flex-col min-h-screen bg-white">
-      {/* Tu propuesta de valor principal */}
-      <Hero />
-
-      {/* Secciones con ID para que el menú sepa a dónde bajar */}
-      <section id="servicios" className="py-20 border-t border-slate-50">
-        <Services />
-      </section>
-
-      <section id="experiencia" className="py-20 bg-slate-50">
-        <Experience />
-      </section>
-
-      <section id="certificaciones" className="py-20">
-        <Certifications />
-      </section>
-
-      <section id="contacto" className="py-20 bg-white">
-        <Contact />
-      </section>
+    <main className="flex flex-col bg-white">
+      <section id="inicio"><Hero /></section>
+      <section id="servicios" className="py-20 border-t border-slate-50"><Services /></section>
+      <section id="experiencia" className="py-20 bg-slate-50"><Experience /></section>
+      <section id="certificaciones" className="py-20"><Certifications /></section>
+      <section id="contacto" className="py-20 bg-white"><Contact /></section>
     </main>
   );
 }
