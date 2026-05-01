ed6f73c2b7f8b6cb8e67f987aac4edf9154d583c
Giovani sanchez vargas <giovanysanchez438@gmail.com>
2026-05-01 14:58:42 -0500

    Refactor Home component to simplify structure
    Removed Certifications and Contact sections from Home component.

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -1,17 +1,13 @@
 import Hero from "@/components/Hero";
 import Services from "@/components/Services";
 import Experience from "@/components/Experience";
-import Certifications from "@/components/Certifications";
-import Contact from "@/components/Contact";
 
 export default function Home() {
   return (
-    <main className="flex flex-col bg-white">
-      <section id="inicio"><Hero /></section>
-      <section id="servicios" className="py-20 border-t border-slate-50"><Services /></section>
-      <section id="experiencia" className="py-20 bg-slate-50"><Experience /></section>
-      <section id="certificaciones" className="py-20"><Certifications /></section>
-      <section id="contacto" className="py-20 bg-white"><Contact /></section>
+    <main>
+      <Hero />
+      <Services />
+      <Experience />
     </main>
   );
 }
