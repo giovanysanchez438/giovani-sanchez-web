afc1e7d343d4e2062d8b5526aa3e3dc351dac780
Giovani sanchez vargas <giovanysanchez438@gmail.com>
2026-05-11 16:31:47 -0500

    Remove Header from Experiencia page
    Removed Header component and related comments.

--- client/src/pages/Experiencia.tsx
+++ client/src/pages/Experiencia.tsx
@@ -1,17 +1,9 @@
-import Header from "@/components/Header";
 import Experience from "@/components/Experience";
 import Contact from "@/components/Contact";
-
-/**
- * Experiencia Page
- * 
- * Diseño: Minimalismo Corporativo Elegante
- */
 
 export default function Experiencia() {
   return (
     <div className="min-h-screen flex flex-col bg-white">
-      <Header />
       <main className="flex-1">
         <Experience />
       </main>
