e6e00e027caf409483e4e5c27d3c4efd429f0ef2
Giovani sanchez vargas <giovanysanchez438@gmail.com>
2026-05-11 16:32:08 -0500

    Remove Header and ContactForm from Contacto page

--- client/src/pages/Contacto.tsx
+++ client/src/pages/Contacto.tsx
@@ -1,21 +1,11 @@
-import Header from "@/components/Header";
-import ContactForm from "@/components/ContactForm";
 import Contact from "@/components/Contact";
-
-/**
- * Contacto Page
- * 
- * Diseño: Minimalismo Corporativo Elegante
- */
 
 export default function Contacto() {
   return (
     <div className="min-h-screen flex flex-col bg-white">
-      <Header />
       <main className="flex-1">
-        <ContactForm />
+        <Contact />
       </main>
-      <Contact />
     </div>
   );
 }
