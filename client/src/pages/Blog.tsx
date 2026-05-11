4c73a9433d935bffae81c4e21b0b48eed771f8c8
Giovani sanchez vargas <giovanysanchez438@gmail.com>
2026-05-11 16:31:26 -0500

    Remove Header component from BlogPage
    Removed Header component and associated comments from BlogPage.

--- client/src/pages/Blog.tsx
+++ client/src/pages/Blog.tsx
@@ -1,17 +1,9 @@
-import Header from "@/components/Header";
 import Blog from "@/components/Blog";
 import Contact from "@/components/Contact";
-
-/**
- * Blog Page
- * 
- * Diseño: Minimalismo Corporativo Elegante
- */
 
 export default function BlogPage() {
   return (
     <div className="min-h-screen flex flex-col bg-white">
-      <Header />
       <main className="flex-1">
         <Blog />
       </main>
