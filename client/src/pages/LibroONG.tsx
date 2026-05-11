5519dfde072d9cabebcc0bd76d22aef0157eb9f1
Giovani sanchez vargas <giovanysanchez438@gmail.com>
2026-05-11 16:53:09 -0500

    Refactor LibroONGPage to use iframe instead of ManusDialog

--- client/src/pages/LibroONG.tsx
+++ client/src/pages/LibroONG.tsx
@@ -1,11 +1,16 @@
-import ManusDialog from "@/components/ManusDialog";
-
-export default function LibroONGPage() {
+export default function LibroONG() {
   return (
-    <div className="min-h-screen flex flex-col bg-white">
-      <main className="flex-1">
-        <ManusDialog />
-      </main>
+    <div style={{ margin: 0, padding: 0, height: "100vh", overflow: "hidden" }}>
+      <iframe
+        src="/libro-finanzas-ong.html"
+        style={{
+          width: "100%",
+          height: "100vh",
+          border: "none",
+          display: "block",
+        }}
+        title="El Arte de Captar Fondos - Giovani Sánchez V."
+      />
     </div>
   );
 }
