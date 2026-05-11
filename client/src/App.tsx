2a7b809422b9bb0b85d8ee5dc91c176855230635
Giovani sanchez vargas <giovanysanchez438@gmail.com>
2026-05-11 16:53:37 -0500

    Refactor route imports and update Router component

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -5,30 +5,28 @@ import ErrorBoundary from "./components/ErrorBoundary";
 import { ThemeProvider } from "./contexts/ThemeContext";
 import Header from "./components/Header";
 
-/* Páginas existentes */
-import Home          from "./pages/Home";
-import Experiencia   from "./pages/Experiencia";
+/* Páginas */
+import Home            from "./pages/Home";
+import Experiencia     from "./pages/Experiencia";
 import Certificaciones from "./pages/Certificaciones";
-import Contacto      from "./pages/Contacto";
-import BlogPage      from "./pages/Blog";
-import LibroONG      from "./pages/LibroONG";
-import NotFound      from "@/pages/NotFound";
-
-/* Páginas nuevas — las creamos a continuación */
-import SobreMi       from "./pages/SobreMi";
-import Trabajo       from "./pages/Trabajo";
+import Contacto        from "./pages/Contacto";
+import BlogPage        from "./pages/Blog";
+import LibroONG        from "./pages/LibroONG";
+import NotFound        from "@/pages/NotFound";
+import SobreMi         from "./pages/SobreMi";
+import Trabajo         from "./pages/Trabajo";
 
 function Router() {
   return (
     <Switch>
-      <Route path="/"             component={Home}           />
-      <Route path="/sobre-mi"     component={SobreMi}        />
-      <Route path="/trabajo"      component={Trabajo}        />
-      <Route path="/experiencia"  component={Experiencia}    />
-      <Route path="/contacto"     component={Contacto}       />
-      <Route path="/blog"         component={BlogPage}       />
-      <Route path="/libro-ong"    component={LibroONG}       />
-      <Route path="/certificaciones" component={Certificaciones} />
+      <Route path="/"                component={Home}           />
+      <Route path="/sobre-mi"        component={SobreMi}        />
+      <Route path="/trabajo"         component={Trabajo}        />
+      <Route path="/experiencia"     component={Experiencia}    />
+      <Route path="/contacto"        component={Contacto}       />
+      <Route path="/blog"            component={BlogPage}       />
+      <Route path="/libro-ong"       component={LibroONG}       />
+      <Route path="/certificaciones" component={Certificaciones}/>
       <Route component={NotFound} />
     </Switch>
   );
